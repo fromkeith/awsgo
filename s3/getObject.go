0ecefcc4845aa06692340fc525bd1e16ffd9e193
fromkeith <keith@fromkeith.com>
2014-01-09 13:17:48 -0500

    Add GetObject to s3 to download files when they need auth... not entirely happy its a []byte right now.. but that can change later.

---
+++ s3/getObject.go
@@ -0,0 +1,134 @@
+/*
+ * Copyright (c) 2013, fromkeith
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ * * Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ * 
+ * * Neither the name of the fromkeith nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package s3
+
+import (
+    "github.com/fromkeith/awsgo"
+    "fmt"
+)
+
+
+type GetObjectRequest struct {
+    awsgo.RequestBuilder
+
+    // headers
+    Range               string
+    IfModifiedSince     string
+    IfUnmodifiedSince   string
+    IfMatch             string
+    IfNoneMatch         string
+
+    // the file
+    Path                string
+}
+
+type GetObjectResponse struct {
+    // headers
+    DeleteMarker        bool
+    Expiration          string
+    Encyption           string
+    Restore             string
+    VersionId           string
+    WebsiteRedirectLocation string
+    // Actual Data
+    Data                []byte
+}
+
+func NewGetObjectRequest() (*GetObjectRequest) {
+    req := new(GetObjectRequest)
+    req.Headers = make(map[string]string)
+    req.RequestMethod = "GET"
+    return req
+}
+
+func (por GetObjectRequest) DeMarshalGetItemResponse(a []byte, headers map[string]string) (interface{}) {
+    if headers == nil {
+        return nil
+    }
+    response := new(GetObjectResponse)
+    if _, ok := headers["x-amz-delete-marker"]; ok {
+        response.DeleteMarker = true // if false, it won't appear according to docs
+    }
+    if v, ok := headers["x-amz-expiration"]; ok {
+        response.Expiration = v
+    }
+    if v, ok := headers["x-amz-server-side​-encryption"]; ok {
+        response.Encyption= v
+    }
+    if v, ok := headers["x-amz-restore"]; ok {
+        response.Restore = v
+    }
+    if v, ok := headers["x-amz-version-id"]; ok {
+        response.VersionId = v
+    }
+    if v, ok := headers["x-amz-website-redirect-location"]; ok {
+        response.WebsiteRedirectLocation = v
+    }
+    response.Data = a
+    return response
+}
+
+func (gi * GetObjectRequest) VerifyInput() (error) {
+    gi.Host.Service = "s3"
+    
+    if gi.Range != "" {
+        gi.Headers["Range"] = gi.Range
+    }
+    if gi.IfModifiedSince != "" {
+        gi.Headers["If-Modified-Since"] = gi.IfModifiedSince
+    }
+    if gi.IfUnmodifiedSince != "" {
+        gi.Headers["If-Unmodified-Since"] = gi.IfUnmodifiedSince
+    }
+    if gi.IfMatch != "" {
+        gi.Headers["If-Match"] = gi.IfMatch
+    }
+    if gi.IfNoneMatch != "" {
+        gi.Headers["If-None-Match"] = gi.IfNoneMatch
+    }
+    
+    gi.CanonicalUri = fmt.Sprintf("/%s", gi.Path)
+    return gi.RequestBuilder.VerifyInput()
+}
+
+
+
+func (gor GetObjectRequest) Request() (*GetObjectResponse, error) {
+    request, err := awsgo.BuildReaderRequest(&gor, nil)
+    if err != nil {
+        return nil, err
+    }
+    request.RequestSigningType = awsgo.RequestSigningType_REST
+    resp, err := awsgo.DoRequest(&gor, request)
+    if resp == nil {
+        return nil, err
+    }
+    return resp.(*GetObjectResponse), err
+}
---
+++ s3/putObject.go
@@ -0,0 +1,147 @@
+/*
+ * Copyright (c) 2013, fromkeith
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ * * Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ * 
+ * * Neither the name of the fromkeith nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package s3
+
+import (
+    "github.com/fromkeith/awsgo"
+    "errors"
+    "fmt"
+    "io"
+    "strings"
+)
+
+
+type PutObjectRequest struct {
+    awsgo.RequestBuilder
+
+    Source io.ReadCloser
+    Length int64
+    ContentType string
+    Permissions string
+    Path string
+}
+
+type PutObjectResponse struct {
+    Hash string
+    RequestId string
+    RequestId2 string
+    VersionId string
+}
+type PutObjectResponseFuture struct {
+    response chan *PutObjectResponse
+    errResponse chan error
+}
+
+func NewPutObjectRequest() (*PutObjectRequest) {
+    req := new(PutObjectRequest)
+    req.Headers = make(map[string]string)
+    req.RequestMethod = "PUT"
+    return req
+}
+
+func (por PutObjectRequest) DeMarshalGetItemResponse(a []byte, headers map[string]string) (interface{}) {
+    if headers == nil {
+        return nil
+    }
+    response := new(PutObjectResponse)
+    if v, ok := headers["etag"]; ok {
+        response.Hash = strings.Trim(v, "\"")
+    }
+    if v, ok := headers["x-amz-id-2"]; ok {
+        response.RequestId2 = v
+    }
+    if v, ok := headers["x-amz-request-id"]; ok {
+        response.RequestId = v
+    }
+    if v, ok := headers["x-amz-version-id"]; ok {
+        response.VersionId = v
+    }
+    return response
+}
+
+func (por * PutObjectRequest) VerifyInput() (error) {
+    por.Host.Service = "s3"
+    if por.Length == 0 {
+        return errors.New("Length cannot be 0")
+    }
+    if len(por.ContentType) == 0 {
+        return errors.New("ContentType be empty")
+    }
+    if len(por.Permissions) == 0 {
+        return errors.New("Permissions be empty")
+    }
+    if len(por.Path) == 0 {
+        return errors.New("Path be empty")
+    }
+    por.Headers["Content-Type"] = por.ContentType
+    por.Headers["x-amz-acl"] = por.Permissions
+    por.Headers["Content-Length"] = fmt.Sprintf("%d", por.Length)
+    por.Headers["Expect"] = "100-continue"
+    por.CanonicalUri = fmt.Sprintf("/%s", por.Path)
+    return por.RequestBuilder.VerifyInput()
+}
+
+
+func (por PutObjectRequest) CoRequest() (*PutObjectResponseFuture, error) {
+    request, err := awsgo.BuildRequest(&por, por.Source)
+    if err != nil {
+        return nil, err
+    }
+    future := new(PutObjectResponseFuture)
+    future.errResponse = make(chan error)
+    future.response = make(chan * PutObjectResponse)
+    go por.CoDoRequest(request, future)
+    return future, nil
+}
+
+func (por PutObjectRequest) CoDoRequest(request awsgo.AwsRequest, future * PutObjectResponseFuture) {
+    request.RequestSigningType = awsgo.RequestSigningType_REST
+    resp, err := awsgo.DoRequest(&por, request)
+    if err != nil {
+        future.errResponse <- err
+    } else {
+        future.response <- resp.(*PutObjectResponse)
+    }
+    close(future.errResponse)
+    close(future.response)
+}
+
+func (por PutObjectRequest) Request() (*PutObjectResponse, error) {
+    request, err := awsgo.BuildReaderRequest(&por, por.Source)
+    if err != nil {
+        return nil, err
+    }
+    request.RequestSigningType = awsgo.RequestSigningType_REST
+    resp, err := awsgo.DoRequest(&por, request)
+    if resp == nil {
+        return nil, err
+    }
+    return resp.(*PutObjectResponse), err
+}
